b171df734d9cf68c09b064b786eab224a1ca78d0
Croix Loïc <croixloic238@laposte.net>
2022-07-20 10:11:17 +0200

    delete comment et update post/comment

--- client/src/components/PostsRead.js
+++ client/src/components/PostsRead.js
@@ -1,57 +1,93 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import PostsCreate from './PostsCreate';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import PostsCreate from "./PostsCreate";
 
 const PostsRead = () => {
+  const [posts, setPosts] = useState([]);
+  const [updated, setUpdated] = useState(false);
+  const [textUpdated, setTextUpdated] = useState(null);
 
-    const [posts, setPosts]= useState([]);
+  const Posts = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}post`)
+      .then((res) => {
+        setPosts(res.data.post);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const Posts = () => {
+  useEffect(() => {
+    Posts();
+  }, []);
 
-        axios.get(`${process.env.REACT_APP_API_URL}post`)
-        .then ((res) =>{
-            setPosts(res.data.post)
-            console.log(res);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+  const handleDelete = (postId) => {
 
-    useEffect (() => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}post/` + postId)
+      .then((res) => {
+        console.log(res);
         Posts();
-    }, [])
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const handleDelete = (postId) => {
-        console.log(postId);
-    
-        axios
-          .delete(`${process.env.REACT_APP_API_URL}post/` + postId)
-          .then((res) => {
-             console.log(res);
-            Posts();
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+   const handleModify = (postId, textUpdated) => {
+    if (textUpdated === "") {
+      alert("veuillez mettre un message !"); 
+    } else {
+     axios 
+     .put(`${process.env.REACT_APP_API_URL}post/` + postId,{
+      textUpdated,
+     })
+       .then((res) => {
+        setUpdated(false);
+        console.log(res);
+         Posts();
+       })
+       .catch((err) => {
+         console.log(err);
+      });
+    }
+   };
 
-    return (
-        
-        <div>
-            <h1> Les posts</h1>
-            {posts.map((element) => (
+  return (
+    <div>
+      <h1> Les posts</h1>
+      <PostsCreate Posts={Posts} />
+      {posts.map((element) => (
         <div className="post">
-          <em>{element.user.firstName} {element.user.lastName}</em>
-          <p>{element.content} {element.date}</p>
+          <em>
+            {element.user.firstName} {element.user.lastName}
+          </em>
+          {updated === false && (
+            <p>
+              {element.content} {element.date}
+            </p>
+          )}
+          {updated && (
+            <div >
+              <textarea
+                defaultValue={element.content}
+                onChange={(e) => setTextUpdated(e.target.value)}
+              />
+              <div>
+                <button className="btn" onClick={() => {
+                     handleModify(element.id)}}>Valider la modification</button>
+              </div>
+            </div>
+          )}
           <Link to={`/post/${element.id}`}>Voir le post</Link>
-          <button onClick={() => handleDelete(element.id)} >Supprimer</button>
+          <button onClick={() => handleDelete(element.id)}>Supprimer</button>
+          <button onClick={() =>{ setUpdated (!updated)}} >Modifier</button>
         </div>
-            ))}
-            <PostsCreate Posts={Posts} />
-
-        </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default PostsRead;
--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -7,6 +7,8 @@ const SinglePost = () => {
   const params = useParams();
 
   const [post, setPost] = useState({});
+  const [updated, setUpdated] = useState(false)
+  const [textUpdated, setTextUpdated] = useState(null);
 
   const Comment = () => {
 
@@ -16,7 +18,7 @@ const SinglePost = () => {
       axios
       .get(`${process.env.REACT_APP_API_URL}comment/` + params.id)
       .then((comment) => {
-        // console.log(comment);
+         console.log(comment);
         setPost({ ...post.data, comments: comment.data });
       });
     });
@@ -25,22 +27,64 @@ const SinglePost = () => {
       Comment();
   }, []);
   
-  console.log(post);
-  const commentDelete = (comment) => {
-    console.log(comment);
-
+  // console.log(post);
+  const commentDelete = (comments) => {
+    console.log(comments);
+    axios.delete(`${process.env.REACT_APP_API_URL}comment/` + comments)
+    .then((res) => {
+      console.log(res);
+      Comment();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
+
+  const commentModify =(comments) => {
+    if (textUpdated === "") {
+      alert("veuillez mettre un message !"); 
+    } else {
+      axios.put(`${process.env.REACT_APP_API_URL}comment/` + comments, {
+        textUpdated
+      }).then((res) => {
+        setUpdated(false);
+        Comment();
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  };
 
   return (
     <>
       <h1>{post.content}</h1>
 
+        < CommenteCreate to={`/post/${params.id}`} commentaire= {Comment}  />
       <ul>
         {post.comments &&
-          post.comments.map((comment) => <li>{comment.content}</li>)}
-      <button onClick={() => commentDelete(params.id)}>Supprimer</button>
+          post.comments.map((comment) => ( 
+          <div className='comment'> 
+          <em>{comment.user.firstName} {comment.user.lastName}</em>
+          {updated === false && (
+            <p>{comment.content}</p>
+          )}
+          {updated && (
+                        <div >
+                        <textarea
+                          defaultValue={comment.content}
+                          onChange={(e) => setTextUpdated(e.target.value)}
+                        />
+                        <div>
+                          <button className="btn" onClick={() => {
+                               commentModify(comment.id)}}>Valider la modification</button>
+                        </div>
+                      </div>
+          )}   
+          <button onClick={() => commentDelete(comment.id)}>Supprimer</button>
+          <button onClick={() => {setUpdated(!updated)}}>Modifier</button>
+          </div>
+          ))}
       </ul>
-      < CommenteCreate to={`/post/${params.id}`} commentaire= {Comment}  />
     </>
   );
 };
