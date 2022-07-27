21d5a83090715065daa73e3fbe71f3fc974d7adc
Croix Loïc <croixloic238@laposte.net>
2022-07-27 10:15:58 +0200

    debut de formData

--- client/src/components/PostsCreate.js
+++ client/src/components/PostsCreate.js
@@ -3,19 +3,33 @@ import axios from "axios";
 
 const PostsCreate = (props) => {
   const [content, setContent] = useState("");
+  const [images, setImages]= useState()
+  
+  const handleFile = (e) =>{
+    setImages ( e.target.files[0])
+  };
   const handleCreate = (e) => {
     e.preventDefault();
+    
+    
+
+    let formData = new FormData();
+
+    formData.append = ('images', images)
+    formData.append = ("content", content)
+
 
     if (content === "") {
       alert("veuillez mettre un message !");
     } else {
-      axios.post(`${process.env.REACT_APP_API_URL}post`, {
-        content,
+      axios.post(`${process.env.REACT_APP_API_URL}post`, {    //content
+        content, images
       }).then (()=> {
         props.Posts()
       })
     }
   };
+
   return (
     <>
       <form action="" onSubmit={handleCreate} id="form-post">
@@ -24,6 +38,7 @@ const PostsCreate = (props) => {
           id="content"
           onChange={(e) => setContent(e.target.value)}
         ></textarea>
+        <input type="file" name="images" id="images" onChange={(e) => {handleFile(e)}} />
         <input type="submit" value="Envoyer"></input>
       </form>
     </>
